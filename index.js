8b1efe5f2b8e7662ed52598cfae2c08f911861ad
Michael Alan Stowe <mastowe94@gmail.com>
2023-05-08 07:27:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(people,event){
+    let messages = []
+    for (let i = 0; i < people.length; i++){
+        messages.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+
+function countDown(num){
+    let x = num
+    while (x > 0){
+      console.log(x)
+        x--;
+    }
+    console.log(x)
+  }
